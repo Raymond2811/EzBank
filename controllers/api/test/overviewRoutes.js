d1becd997e5a37333923e8dcef601ac0769028c6
JosueHernand <joseh1337@gmail.com>
2023-11-14 02:12:34 -0800

    Display checking overview account

--- controllers/api/test/overviewRoutes.js
+++ controllers/api/test/overviewRoutes.js
@@ -1,3 +1,34 @@
-// Will have 'get' inside here for account checkings
-// Display transactions
-// Account overview file
+const router = require('express').Router();
+const { AccOverview, Checkings } = require('../../../models');
+const withAuth = require('../../../utils/auth');
+
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        const overviewData = await AccOverview.findOne({
+            where: {
+                Checkings_id: req.params.id,
+            },
+            include: [
+                {
+                    model: Checkings,
+                    attributes: ['id', 'name', 'balance'],
+                },
+            ],
+        });
+
+        if (!overviewData) {
+            return res.render('error', { message: 'No overview found with this id!' });
+        }
+
+        const overview = overviewData.get({ plain: true });
+
+        res.render('overview', {
+            ...overview,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
