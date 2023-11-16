fd3198932628f94efe0284e3adbf44ccf148d4ec
JosueHernand <joseh1337@gmail.com>
2023-11-15 20:54:06 -0800

    Changed all of homeRoutes for front page to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,35 +1,11 @@
 const router = require('express').Router();
-const { Checkings, User } = require('../models');
+const { Homepage, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/', async (req, res) => {
 
-//     // Serialize data so the template can read it
-//     const projects = projectData.map((project) => project.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       projects, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-router.get('/Checking/:id', async (req, res) => {
   try {
-    const checkingsData = await Checkings.findByPk(req.params.id, {
+    const homeData = await Homepage.findAll({
       include: [
         {
           model: User,
@@ -38,10 +14,33 @@ router.get('/Checking/:id', async (req, res) => {
       ],
     });
 
-    const Checkings = checkingsData.get({ plain: true });
+    const homePage = homeData.map((homepageEntry) => homepageEntry.get({ plain: true }));
 
-    res.render('Checkings', {
-      ...Checkings,
+    res.render('homepage', { 
+      homePage, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+
+  try {
+    const homeData = await Homepage.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = homeData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -49,13 +48,12 @@ router.get('/Checking/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
+
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Checkings }],
+      include: [{ model: Homepage }],
     });
 
     const user = userData.get({ plain: true });
@@ -70,7 +68,7 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
