fcc8f92984dfb29c14f3cee7138f3ad0f25aedb6
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-20 23:48:35 -0800

    renamed index file and updated routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.redirect('/homepage');
+    res.redirect('/public/homepage');
 });
 
 router.get('/homepage', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/homepage.html'));
+    res.sendFile(path.join(__dirname, '../public/html/homepage.html'));
 });
   
 router.get('/aboutus', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/about.html'));
+    res.sendFile(path.join(__dirname, '../public/html/about.html'));
 });
 
 router.get('/signup', (req, res) => {
