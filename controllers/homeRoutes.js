a7b24269176e73f203a22f442be891f77f4e147f
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-22 23:38:36 -0800

    added 2 endpoints

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const path = require('path');
+const {User, Checkings} = require('../models');
 
 router.get('/', (req, res) => {
     res.redirect('/homepage');
@@ -17,5 +18,42 @@ router.get('/signup', (req, res) => {
     res.render('enroll');
 });
 
+router.get('/bankaccount', async (req, res) => {
+    try {
+      const CheckingData = await User.findByPk(req.session.user_id, {
+        include: [{ model: Checkings }],
+      });
+      
+      if (!CheckingData) {
+        res.status(404).json({ message: 'No Checking found with this id!' });
+        return;
+      }
+      const checkingData = CheckingData.get({plain:true});
+      console.log(checkingData);
+      res.render('checking',checkingData);
+    } catch (error) {
+      console.log(error);  
+      res.status(500).json(error);
+    }
+  });
 
+  router.get('/checking/:id', async (req, res) => {
+    try {
+      const CheckingData = await Checkings.findByPk(req.params.id, {
+        // include: [{ model: Checkings }],
+      });
+      
+      if (!CheckingData) {
+        res.status(404).json({ message: 'No Checking found with this id!' });
+        return;
+      }
+      const checkingData = CheckingData.get({plain:true});
+      console.log(checkingData);
+      res.render('AccOverview',checkingData);
+    } catch (error) {
+      console.log(error);  
+      res.status(500).json(error);
+    }
+  });
+  
 module.exports = router;
