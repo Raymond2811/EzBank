d47b533f7c5489a776de0cdf65a918a71273b8bf
JosueHernand <joseh1337@gmail.com>
2023-11-23 02:32:23 -0800

    Made the login button work

---
+++ controllers/api/bankAccount/index.js
@@ -0,0 +1,8 @@
+// const router = require('express').Router();
+// const bankAccountRoutes = require('../bankAccountRoutes');
+// const overview = require('./overview');
+
+// router.use('/',bankAccountRoutes);
+// router.use('/accountoverview',overview);
+
+// module.exports = router;
