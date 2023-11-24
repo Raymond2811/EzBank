5ecf0db352ba51d26583b0149bf8e5f4fe3f9a95
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-24 02:16:42 -0800

    added spanish feature and fixed minor errors

---
+++ public/js/language.js
@@ -0,0 +1,60 @@
+let currentLanguage = 'english';
+
+function toggleLanguage() {
+  currentLanguage = currentLanguage === 'english' ? 'spanish' : 'english';
+
+  updateLinkText();
+  updateAboutUsLink();
+  updateMainContent();
+  updateFooterContent();
+}
+
+function updateLinkText() {
+  const link = document.getElementById('languageLink');
+  link.innerText = currentLanguage === 'english' ? 'Español' : 'English';
+}
+
+const aboutUsLinkText = {
+  english: 'About Us',
+  spanish: 'Sobre Nosotros'
+};
+
+function updateAboutUsLink() {
+  const aboutUsLink = document.getElementById('aboutUsLink');
+  aboutUsLink.innerText = aboutUsLinkText[currentLanguage];
+}
+
+const mainContent = {
+  english: 'englishContent',
+  spanish: 'spanishContent'
+};
+
+function updateMainContent() {
+  Object.keys(mainContent).forEach((language) => {
+    document.getElementById(mainContent[language]).style.display = 'none';
+  });
+  document.getElementById(mainContent[currentLanguage]).style.display = 'block';
+}
+
+const footerContent = {
+  english: {
+    contactHeader: 'Contact Us',
+    contactUsText: 'For inquiries and assistance, feel free to reach out:',
+    connectHeader: 'Connect With Us'
+  },
+  spanish: {
+    contactHeader: 'Contáctenos',
+    contactUsText: 'Para consultas y asistencia, no dude en comunicarse:',
+    connectHeader: 'Conéctese con nosotros'
+  }
+};
+
+function updateFooterContent() {
+  const contactHeader = document.getElementById('contactHeader');
+  const contactUsText = document.getElementById('contactUsText');
+  const connectHeader = document.getElementById('connectHeader');
+
+  contactHeader.innerText = footerContent[currentLanguage].contactHeader;
+  contactUsText.innerText = footerContent[currentLanguage].contactUsText;
+  connectHeader.innerText = footerContent[currentLanguage].connectHeader;
+}
