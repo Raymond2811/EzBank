341501da554e60f15be1b4e2757a87738d4f09c0
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-24 02:24:07 -0800

    added prettier

--- models/AccOverview.js
+++ models/AccOverview.js
@@ -1,45 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class AccOverview extends Model {}
 
 AccOverview.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: 'EzBank Checking Account',
-        },
-        balance: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-        transactions:{
-            type: DataTypes.STRING,
-            defaultValue: '',
-        },
-        checkings_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Checkings',
-                key: 'id',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'AccOverview',
-      }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "EzBank Checking Account",
+    },
+    balance: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    transactions: {
+      type: DataTypes.STRING,
+      defaultValue: "",
+    },
+    checkings_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Checkings",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "AccOverview",
+  },
 );
 
 module.exports = AccOverview;
